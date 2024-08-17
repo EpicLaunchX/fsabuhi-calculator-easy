cd75e73ebf87a0536eb03bb5a40378c86b899c9c
fsabuhi <93775724+fsabuhi@users.noreply.github.com>
2024-08-17 13:06:14 +0400

    fix: added mock to test function body

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,28 +1,28 @@
 import src.pytemplate.entrypoints.cli.main as cli
 
 
-def test_add(monkeypatch):
+def mock_add(monkeypatch):
     inputs = iter(["1", "2", "add"])
     monkeypatch.setattr("builtins.input", lambda: next(inputs))
     result = cli.main()
     assert result == 3
 
 
-def test_subtract(monkeypatch):
+def mock_subtract(monkeypatch):
     inputs = iter(["1", "2", "subtract"])
     monkeypatch.setattr("builtins.input", lambda: next(inputs))
     result = cli.main()
     assert result == -1
 
 
-def test_divide(monkeypatch):
+def mock_divide(monkeypatch):
     inputs = iter(["1", "2", "multiply"])
     monkeypatch.setattr("builtins.input", lambda: next(inputs))
     result = cli.main()
     assert result == 2
 
 
-def test_multiply(monkeypatch):
+def mock_multiply(monkeypatch):
     inputs = iter(["1", "2", "divide"])
     monkeypatch.setattr("builtins.input", lambda: next(inputs))
     result = cli.main()
