28544fda92873fd9bafd4bf29d40158513d78891
fsabuhi <93775724+fsabuhi@users.noreply.github.com>
2024-08-17 13:11:19 +0400

    fix: add unittest.mock

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -1,29 +1,31 @@
+from unittest import mock
+
 import src.pytemplate.entrypoints.cli.main as cli
 
 
-def mock_add(monkeypatch):
+def test_add(monkeypatch):
     inputs = iter(["1", "2", "add"])
-    monkeypatch.setattr("builtins.input", lambda: next(inputs))
+    mock.patch("builtins.input", lambda: next(inputs))
     result = cli.main()
     assert result == 3
 
 
-def mock_subtract(monkeypatch):
+def test_subtract(monkeypatch):
     inputs = iter(["1", "2", "subtract"])
-    monkeypatch.setattr("builtins.input", lambda: next(inputs))
+    mock.patch("builtins.input", lambda: next(inputs))
     result = cli.main()
     assert result == -1
 
 
-def mock_divide(monkeypatch):
+def test_divide(monkeypatch):
     inputs = iter(["1", "2", "multiply"])
-    monkeypatch.setattr("builtins.input", lambda: next(inputs))
+    mock.patch("builtins.input", lambda: next(inputs))
     result = cli.main()
     assert result == 2
 
 
-def mock_multiply(monkeypatch):
+def test_multiply(monkeypatch):
     inputs = iter(["1", "2", "divide"])
-    monkeypatch.setattr("builtins.input", lambda: next(inputs))
+    mock.patch("builtins.input", lambda: next(inputs))
     result = cli.main()
     assert result == 0.5
